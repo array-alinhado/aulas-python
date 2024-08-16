6f4f98a444d2871aa17cb1fab91f90884647528a
Itauã.Borges <anonimobomdia@gmail.com>
2024-08-16 17:50:38 +0000

    tipagem dinamica.py

---
+++ tipos primitivos.py
@@ -0,0 +1,13 @@
+# tipagem estatica
+nome : str # cadeia
+idade : int # inteiro
+altura : float # real
+
+
+nome = "marta"
+idade = 20
+altura = 1.65
+
+print (f"nome:  {nome}")
+print (f"idade:  {idade}")
+print (f"altura: {altura}")
