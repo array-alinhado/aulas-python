c3623848817ac7240d8d76f77a0fe308405717c7
Itauã.Borges <anonimobomdia@gmail.com>
2024-08-21 16:51:59 +0000

    aulas-phyton

---
+++ 2. estruturas condicionais/atividade3.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls || clear")
+
+usuario = input("digite seu nome: ")
+senha = float (input("digite sua senha: "))
+
+if usuario != "itau" or senha != 6969:
+    print("senha incorreta!!!")
+else:
+    print("bem-vindo!!!")
