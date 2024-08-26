b47ba51734be7559c793c29046c76e70745a8a18
Itauã.Borges <anonimobomdia@gmail.com>
2024-08-26 17:48:22 +0000

    aulas-python

---
+++ interation/atividade3.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls || clear")
+
+soma = 0
+media = 0
+
+for i in range (4):
+    notas = float(input("digite sua media: "))
+
+soma = soma + notas
+media = media + notas 
+
+
+print(f"media: {media}")
