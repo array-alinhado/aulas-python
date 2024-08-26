f7f7d8521f312503b94050ed4aef53e8d57efc92
Itauã.Borges <anonimobomdia@gmail.com>
2024-08-26 19:52:13 +0000

    aula-python

---
+++ interation/atividade6.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls || clear")
+import time
+
+numero = int(input("digite seu numero: "))
+
+for i in range(numero, 0 , -1):
+        print(i)
+        time.sleep(1)
+
+print("fimmmmmmm")
